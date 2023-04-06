25f9ef3635da3aeb8293631092e048a57568bb1e
ayndqy <ayndqy@gmail.com>
2023-04-06 16:12:54 +0100

    Fix CMakeLists.txt

--- src/level_setup.c
+++ src/level_setup.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
-const char alphanum_arr[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                               'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0',
-                               '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char alphanum_arr[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                         'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0',
+                         '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-const char* morse_alphanum_arr[] = {
+char* morse_alphanum_arr[] = {
     ".-",    "-...",  "-.-.",  "-..",   ".",     "..-.",  "--.",   "....",
     "..",    ".---",  "-.-",   ".-..",  "--",    "-.",    "---",   ".--.",
     "--.-",  ".-.",   "...",   "-",     "..-",   "...-",  ".--",   "-..-",
     "-.--",  "--..",  "-----", ".----", "..---", "...--", "....-", ".....",
     "-....", "--...", "---..", "----."};
 
-const char* word_arr[] = {"HELLO",  "WORLD",  "PICO", "SCALA",  "C",
-                          "PYTHON", "SUFFER", "RUST", "ENDURE", "JAVA"};
+char* word_arr[] = {"HELLO",  "WORLD",  "PICO", "SCALA",  "C",
+                    "PYTHON", "SUFFER", "RUST", "ENDURE", "JAVA"};
 
-const char* morse_word_arr[] = {".... . .-.. .-.. ---",
-                                ".-- --- .-. .-.. -..",
-                                ".--. .. -.-. ---",
-                                "...- .-.-.- .-.. .-.. .",
-                                "-.-. .",
-                                ".--. -.-- --- -. -.--",
-                                "...- ..-. --- .-. .- -..",
-                                ".-. .. ... ...- .",
-                                ". -.. .- -. -.. .",
-                                ".--- .- ...- .-"};
+char* morse_word_arr[] = {".... . .-.. .-.. ---",
+                          ".-- --- .-. .-.. -..",
+                          ".--. .. -.-. ---",
+                          "...- .-.-.- .-.. .-.. .",
+                          "-.-. .",
+                          ".--. -.-- --- -. -.--",
+                          "...- ..-. --- .-. .- -..",
+                          ".-. .. ... ...- .",
+                          ". -.. .- -. -.. .",
+                          ".--- .- ...- .-"};
 
 char* level1() {
   int index = rand() % 36;
-  char* task_letter = alphanum_arr[index];
+  char* task_letter = &alphanum_arr[index];
   char* task_morse = morse_alphanum_arr[index];
   printf("Level 1: %s or &s\n", task_letter, task_morse);
   return task_letter;
@@ -39,7 +40,7 @@ char* level1() {
 
 char* level2() {
   int index = rand() % 36;
-  char* task_letter = alphanum_arr[index];  
+  char* task_letter = &alphanum_arr[index];
   printf("Level 2: &s\n", task_letter);
   return task_letter;
 }
