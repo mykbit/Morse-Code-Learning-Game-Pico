884727d347c99f4ebfdcf9b47085922bc01da1a6
bitiutsm <bitiutsm@tcd.ie>
2023-04-10 03:02:26 +0100

    Rework a function that plays the level #4

---
+++ src/input_evaluate.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+bool evaluate_input(char *task, char input, int index);
---
+++ src/level_setup.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+
+char* level1();
+
+char* level2();
+
+char* level3();
+
+char* level4();
